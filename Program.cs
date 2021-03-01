dbb1a37635e9fd41889cd0782536f24fe649689a
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-02 04:42:48 +0600

    Main Method

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
+            Bank bank = new Bank("Personal Bank", 5);
+            bank.AddAccount(new Account(1001, "Rahim", 20000, new Address("2", "101/A", "Dhaka", "Bangladesh")));
+            bank.AddAccount(new Account(1002, "Karim", 40000, new Address("4", "102/B", "Barisal", "Bangladesh")));
+            bank.AddAccount(new Account(1003, "Jamal", 60000, new Address("6", "202/C", "Rajshahi", "Bangladesh")));
+            bank.AddAccount(new Account(1004, "Jobbar", 80000, new Address("8", "111/G", "Sylhet", "Bangladesh")));
+            bank.AddAccount(new Account(1006, "Hasan", 120000, new Address("12", "800/K", "Khulna", "Bangladesh")));
+            bank.PrintAllAccountDetails();
         }
     }
 }
