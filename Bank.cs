66c242682a50da696c9c991f2658523bfdcfa884
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-02 04:32:40 +0600

    5

---
+++ Bank.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Bank
+    {
+        public string bankName;
+        public Account[] myBank;
+
+        public Bank(string name, int size)
+        {
+            this.bankName = name;
+            myBank = new Account[size];
+        }
+        public string BankName
+        {
+            set { this.bankName = value; }
+            get { return this.bankName; }
+        }
+        public Account[] MyBank
+        {
+            set { this.myBank = value; }
+            get { return this.myBank; }
+        }
+
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    myBank[i] = account;
+                    break;
+                }
+            }
+        }
+        public void DeleteAccount(Account account)
+        {
+            for (int j = 0; j < myBank.Length; j++)
+            {
+                if (myBank[j] == account)
+                {
+                    myBank[j] = null;
+                    break;
+                }
+                Console.WriteLine("Account Deleted Successfully!");
+            }
+        }
+
+        public void PrintAllAccountDetails()
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    continue;
+                }
+                myBank[i].ShowAccountInformation();
+            }
+        }
+    }
+}
