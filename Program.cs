3ad56cd5231165b2d834fcedd0d17eb1bd5e41bf
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-02 14:53:11 +0600

    NEW

--- Program.cs
+++ Program.cs
@@ -15,16 +15,19 @@ namespace BankApplication
 
             Bank bank = new Bank("Personal Bank", 5);
             bank.AddAccount(new Account(1, "Rahim", 20000, new Address("2", "101/A", "Dhaka", "Bangladesh")));
-            //bank.AddAccount(new Account(accountNo+1, "Karim", 40000, new Address("4", "102/B", "Barisal", "Bangladesh")));
-            //bank.AddAccount(new Account(accountNo+2, "Jamal", 60000, new Address("6", "202/C", "Rajshahi", "Bangladesh")));
-            //bank.AddAccount(new Account(accountNo+3, "Jobbar", 80000, new Address("8", "111/G", "Sylhet", "Bangladesh")));
-            //bank.AddAccount(new Account(accountNo+4, "Hasan", 120000, new Address("12", "800/K", "Khulna", "Bangladesh")));
+            bank.AddAccount(new Account(2, "Karim", 40000, new Address("4", "102/B", "Barisal", "Bangladesh")));
+            bank.AddAccount(new Account(3, "Jamal", 60000, new Address("6", "202/C", "Rajshahi", "Bangladesh")));
+            bank.AddAccount(new Account(4, "Jobbar", 80000, new Address("8", "111/G", "Sylhet", "Bangladesh")));
 
             //Transactions:
             bank.Transaction(1, bank.myBank[0], 500);
             bank.Transaction(2, bank.myBank[0], 100);
+            bank.Transaction(2, bank.myBank[1], 400);
+            bank.Transaction(1, bank.myBank[1], 100);
+
+            //Print Details:
             bank.PrintAllAccountDetails();
-            bank.DeleteAccount(bank.MyBank[2]);
+            bank.DeleteAccount(bank.MyBank[3]);
         }
     }
 }
