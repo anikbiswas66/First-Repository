8ef1f9ef5d01ce46ecb5e2e3a2acafabbb546d89
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-08 22:14:10 +0600

    Add Savings Account

--- SavingsAccount.cs
+++ SavingsAccount.cs
@@ -6,14 +6,39 @@ using System.Threading.Tasks;
 
 namespace BankApplication
 {
-        class SavingsAccount : Account
+    class SavingsAccount:Account
+    {
+        public SavingsAccount(int accountNumber, string accountName, double balance, string dob, string address, int accountType):base(accountNumber, accountName, balance, dob, address)
         {
-            public SavingsAccount(AccountHolder accountHolder, int accountNumber, double balance) : base(accountNumber, accountHolder, balance)
+            this.accountNumber = accountNumber;
+            this.accountName = accountName;
+            this.balance = balance;
+            this.dob = dob;
+            this.address = address;
+        }
+        public void Withdraw(double amount)
+        {
+            if (base.balance > amount && amount > 0)
             {
-                AccountNumber = accountNumber;
-                Type = AccountType.Savings;
-                Balance = balance;
-                AccountHolder = accountHolder;
+                base.Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Sorry Boss, Not Sufficient Balance");
             }
         }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                base.Balance += amount;
+            }
+            else 
+            { 
+                Console.WriteLine("Sorry Boss, Can't Accept!");
+            }
+        }
+    }
 }
+
