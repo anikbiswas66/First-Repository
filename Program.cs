4579ea7fcd86e2ba4522d8973ba1e80e99bfa7d8
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-09 00:46:39 +0600

    Main

--- Program.cs
+++ Program.cs
@@ -8,25 +8,105 @@ namespace BankApplication
 {
     class Program
     {
-        //Random rnd = new Random();
-        //int accountNo = rnd.Next(1, 1000);
         static void Main(string[] args)
         {
-            string choice; 
+            string choice, accountName, dob, address;
+            int accountNumber;
+            string balance;
+
+            Random rnd = new Random();
+            int accountNo = rnd.Next(100, 1000);
+            accountNo++;
+
             Bank bank = new Bank("YO Bank", 1000);
             Console.WriteLine("___________________________");
             Console.WriteLine("Welcome to YO Bank!");
             Console.WriteLine("___________________________");
 
-            for(int i = 0; i<10; i++)
+            //for(int i = 0; i<10; i++)
+            //{
+            bool repeat = true;
+            while(repeat)
             {
                 Console.WriteLine("Here are your Options: ");
-                Console.WriteLine("Open a Bank account");
-                Console.WriteLine("Perform transactions for an account");
-                Console.WriteLine("Exit the application");
+                Console.WriteLine("1. Open a Bank account");
+                Console.WriteLine("2. Perform transactions for an account");
+                Console.WriteLine("3. Exit the application");
                 choice = Console.ReadLine();
 
+                switch(choice)
+                {
+                    case "open":
+                        {
+                            Console.WriteLine("Open Savings Account");
+                            Console.WriteLine("Open Checking Account");
+                            Console.WriteLine("Exit from the application");
+                            string choice1 = Console.ReadLine();
 
+                            if (choice1 == "quit")
+                            {
+                                break;
+                            }
+                            else if (choice1 == "savings")
+                            {
+                                Console.WriteLine("Enter Your Name : ");
+                                accountName = Console.ReadLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter Date of Birth :");
+                                dob = Console.ReadLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter Adress :");
+                                address = Console.ReadLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter Balance :");
+                                string balance1 = Console.ReadLine();
+                                int b1 = Convert.ToInt32(balance1);
+                                Console.WriteLine();
+
+                                SavingsAccount sa = new SavingsAccount(accountNo, accountName, b1, dob, address);
+                            }
+                            else if(choice == "checking")
+                            {
+                                Console.WriteLine("Enter Your Name : ");
+                                accountName = Console.ReadLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter Date of Birth :");
+                                dob = Console.ReadLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter Adress :");
+                                address = Console.ReadLine();
+                                Console.WriteLine();
+
+                                Console.WriteLine("Enter Balance :");
+                                string balance1 = Console.ReadLine();
+                                int b1 = Convert.ToInt32(balance1);
+                                Console.WriteLine();
+                            }
+                            break;
+                        }
+                    case "account":
+                        {
+                            Console.WriteLine("Make a deposit");
+                            Console.WriteLine("Make a withdrawal");
+                            Console.WriteLine("Make a transfer");
+                            Console.WriteLine("Show the number transactions and balance");
+                            Console.WriteLine("Exit the application");
+                            string choice2 = Console.ReadLine();
+
+                            if (choice2 == "quit")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                bank.PerformTransactions(1, accountNo);
+                            }
+                        }
             }
         }
     }
